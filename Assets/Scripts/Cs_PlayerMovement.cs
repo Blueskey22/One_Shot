39170d0f56a1320153248c8bdeb2b0e9b9ae2c0c
PcCom\Jaime Hall <jaime.hall.007@gmail.com>
2018-10-09 19:45:29 +0200

    Player_mov_x

---
+++ Assets/Scripts/Cs_PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cs_PlayerMovement : MonoBehaviour {
+    private Vector2 _newspeed;
+    public float _speed;
+    public float _acceleration;
+    private Rigidbody2D _rb;
+	// Use this for initialization
+	void Start () {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+
+	void FixedUpdate () {
+        Debug.Log(Input.GetAxis("Horizontal"));
+            
+        _newspeed = _rb.velocity;
+        _newspeed.x = _newspeed.x + Input.GetAxis("Horizontal") * _acceleration * Time.deltaTime;
+        _rb.velocity = _newspeed;
+        
+	}
+   
+}
